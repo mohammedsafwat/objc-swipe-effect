bc0a43dd8d4e5b4fb7e37b4ea4e94286f164bf8b
mohammedsafwat <mail.msafwat@gmail.com>
2015-07-03 04:27:47 +0200

    Initial Commit

---
+++ SwipeEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeEffect
+//
+//  Created by Mohammad Safwat on 7/3/15.
+//  Copyright (c) 2015 Spyros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeEffect
+//
+//  Created by Mohammad Safwat on 7/3/15.
+//  Copyright (c) 2015 Spyros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
